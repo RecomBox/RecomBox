38ab5d0d522fa24d1873b3eff0cd09089e9e52f3
GoodDay360 <istartgame31@gmail.com>
2026-05-03 03:36:26 +0700

    fix: fix udpater

--- lib/src/global/workers/check_update_worker.dart
+++ lib/src/global/workers/check_update_worker.dart
@@ -6,6 +6,10 @@ import 'package:recombox/src/global/app_color.dart';
 import 'package:recombox/src/rust/method/check_update.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import 'package:ota_update/ota_update.dart';
+
+import 'dart:io';
+
 Future<void> checkUpdateWorker() async {
   while (true) {
     final context = navigatorKey.currentContext;
@@ -15,73 +19,124 @@ Future<void> checkUpdateWorker() async {
         
         if (!context.mounted) return;
         if(checkResult != null){
-          debugPrint(checkResult.toString());
-          AppColorsScheme appColors = appColorsNotifier.value;
           showDialog(
             context: context,
-            builder: (_) => AlertDialog(
-              backgroundColor: appColors.tertiary,
-              title: Column(
-                spacing: 15,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'New update available v${checkResult.latestVersion}',
-                    style: GoogleFonts.nunito(
-                      color: appColors.textPrimary,
-                      fontSize: 28,
-                      fontWeight: FontWeight(800),
+            barrierDismissible: false,
+            builder: (_) {
+
+              bool isDownloading = false;
+              double downloadProgress = 0;
+              AppColorsScheme appColors = appColorsNotifier.value;
+              return StatefulBuilder( 
+                builder: (context, setState) {
+                  return AlertDialog(
+                    backgroundColor: appColors.tertiary,
+                    title: Column(
+                      spacing: 15,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'New update available v${checkResult.latestVersion}',
+                          style: GoogleFonts.nunito(
+                            color: appColors.textPrimary,
+                            fontSize: 28,
+                            fontWeight: FontWeight(800),
+                          ),
+                          textAlign: TextAlign.start,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            isDownloading ? 'Updating...' : 'Update Now?',
+                            style: GoogleFonts.nunito(
+                              color: appColors.textSecondary,
+                              fontSize: 24,
+                              fontWeight: FontWeight(600),
+                            ),
+                            textAlign: TextAlign.end,
+                          )
+                        ),
+                      ],
                     ),
-                    textAlign: TextAlign.start,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Update Now?',
-                      style: GoogleFonts.nunito(
-                        color: appColors.textSecondary,
-                        fontSize: 24,
-                        fontWeight: FontWeight(600),
-                      ),
-                      textAlign: TextAlign.end,
-                    )
-                  ),
-                ],
-              ),
-              
-              actions: [
-                TextButton(
-                  child: Text(
-                    'No',
-                    style: GoogleFonts.nunito(
-                      color: appColors.textPrimary,
-                      fontWeight: FontWeight(800),
+                    
+                    actions: [
+                      if (!isDownloading) ...[
+                        TextButton(
+                          child: Text(
+                            'No',
+                            style: GoogleFonts.nunito(
+                              color: appColors.textPrimary,
+                              fontWeight: FontWeight(800),
 
-                    )
-                  ),
-                  onPressed: () => Navigator.pop(context),
-                ),
-                TextButton(
-                  child: Text(
-                    'Yes',
-                    style: GoogleFonts.nunito(
-                      color: appColors.textPrimary,
-                      fontWeight: FontWeight(800),
+                            )
+                          ),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        TextButton(
+                          child: Text(
+                            'Yes',
+                            style: GoogleFonts.nunito(
+                              color: appColors.textPrimary,
+                              fontWeight: FontWeight(800),
 
-                    )
-                  ),
-                  onPressed: () {
-                    launchUrl(
-                      Uri.parse(checkResult.downloadUrl),
-                      mode: LaunchMode.platformDefault,
-                    )
-                      .then((value) => debugPrint(value.toString()))
-                      .catchError((error) => debugPrint(error.toString()));
-                  },
-                ),
-              ],
-            ),
+                            )
+                          ),
+                          onPressed: () {
+                            if (Platform.isAndroid){
+                              debugPrint(checkResult.downloadUrl.trim());
+                              setState((){
+                                isDownloading = true;
+                              });
+                              OtaUpdate()
+                                .execute(
+                                  checkResult.downloadUrl,
+
+                                ).listen(
+                                  (OtaEvent event) {
+                                    if (event.status == OtaStatus.DOWNLOADING) {
+                                      debugPrint(event.value.toString());
+                                      setState((){
+                                        isDownloading = true;
+                                        downloadProgress = double.parse(event.value??"0")/100;
+                                      });
+                                    } else if (event.status == OtaStatus.INSTALLING) {
+                                      setState((){
+                                        isDownloading = false;
+                                      });
+                                    } else if (event.status == OtaStatus.CHECKSUM_ERROR) {
+                                      setState((){
+                                        isDownloading = false;
+                                      });
+                                    }
+                                  },
+                                );
+                            }else{
+                              launchUrl(
+                                Uri.parse(checkResult.downloadUrl),
+                                mode: LaunchMode.platformDefault,
+                              )
+                                .then((value) => debugPrint(value.toString()))
+                                .catchError((error) => debugPrint(error.toString()));
+                            }
+                          },
+                        ),
+                      
+                      ],
+                      if (isDownloading) 
+                        SizedBox(
+                          height: 5,
+                          child: LinearProgressIndicator(
+                            value: downloadProgress,
+                            color: appColors.accentSecondary,
+                          ),
+                        )
+                    ],
+                  );
+                
+                }
+              );
+            }
           );
         }
 
